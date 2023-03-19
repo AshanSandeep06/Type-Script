39f4d20c60f73c80e1e833755beea7f98268436e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-19 15:18:20 +0530

    number dataType has been introduced to index.ts file

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
 console.log("Hello World..!");
+/* ----- Data Types in TypeScript ----- */
+// 1.number
+let value1 = 10;
+let value2 = 15.80;
+console.log(value1 + " " + value2);
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ console.log("Hello World..!");
 /* ----- Data Types in TypeScript ----- */
 
 // 1.number
+let value1: number = 10;
+let value2: number = 15.80;
+console.log(value1 + " " + value2);
